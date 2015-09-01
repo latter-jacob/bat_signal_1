d79e3bbecc3c6d438ce28174602a8b0308c8cfed
latter-jacob <latter.jacob@gmail.com>
2015-09-01 18:56:01 -0400

    attempting to go back to last working state

--- server.rb
+++ server.rb
@@ -1,11 +1,10 @@
-require "puma"
+require "pry"
 require 'sinatra'
+require 'tropo-webapi-ruby'
 require "pg"
-require 'rubygems' # not necessary with ruby 1.9 but included for completeness
-require 'twilio-ruby'
+
 
 configure :development do
-  require 'dotenv'
   set :db_config, { dbname: "bat_signal" }
 end
 
@@ -29,24 +28,12 @@ def db_connection
   end
 end
 
-
 get "/" do
-  redirect '/batsignal'
+ redirect "/batsignal"
 end
+
+
 
 get "/batsignal" do
   erb :index
 end
-
-Twilio.configure do |config|
-  config.account_sid = 'TWILIO_ACCOUNT_SID'
-  config.auth_token = 'TWILIO_AUTH_TOKEN'
-end
-# set up a client to talk to the Twilio REST API
-@client = Twilio::REST::Client.new account_sid, auth_token
-
-@client.account.messages.create({
-    :from => '+15183175026',
-    :to => '+15183399563',
-    :body => 'To The Batmobile!',
-})
