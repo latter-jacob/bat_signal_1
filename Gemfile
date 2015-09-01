895dcfe124c925a6af7b0935212972de81d3610c
latter-jacob <latter.jacob@gmail.com>
2015-09-01 14:39:25 -0400

    more heroku

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+ruby "2.1.5"
+
+gem "sinatra"
+gem "puma"
+gem "pg"
--- server.rb
+++ server.rb
@@ -1,6 +1,20 @@
 require "pry"
 require 'sinatra'
 require 'tropo-webapi-ruby'
+require "pg"
+
+configure :development do
+  set :db_config, { dbname: "bat_signal" }
+end
+
+def db_connection
+  begin
+    connection = PG.connect(settings.db_config)
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
 
 get "/" do
  redirect "/batsignal"
