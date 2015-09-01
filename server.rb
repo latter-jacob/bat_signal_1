1d800aa069464e8296096029a4872b779c06178a
latter-jacob <latter.jacob@gmail.com>
2015-09-01 15:15:02 -0400

    app primed for deployment

--- server.rb
+++ server.rb
@@ -28,20 +28,12 @@ def db_connection
   end
 end
 
-get "/" do
- redirect "/batsignal"
-end
-
-
-
-get "/batsignal" do
-  erb :index
-end
-
-
-post '/index.json' do
-  t = Tropo::Generator.new
-  t.call(:to => "PHONE_NUMBER", :network => "SMS")
-  t.say(:value => "Tag, you're it!")
-  t.response
-end
+# get "/" do
+#  redirect "/batsignal"
+# end
+#
+#
+#
+# get "/batsignal" do
+#   erb :index
+# end
